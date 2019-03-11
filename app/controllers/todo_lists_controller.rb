88d3227b6fc3e1c2373815a34cfcbb87abc1ead8
melzreal <melissa.real@hotmail.com>
2019-03-11 22:09:48 +0000

    finished tutorial, needs ability to untick completed items

--- app/controllers/todo_lists_controller.rb
+++ app/controllers/todo_lists_controller.rb
@@ -1,34 +1,31 @@
 class TodoListsController < ApplicationController
   before_action :set_todo_list, only: [:show, :edit, :update, :destroy]
 
-  # GET /todo_lists
-  # GET /todo_lists.json
+  
   def index
     @todo_lists = TodoList.all
   end
 
-  # GET /todo_lists/1
-  # GET /todo_lists/1.json
+
   def show
   end
 
-  # GET /todo_lists/new
+ 
   def new
     @todo_list = TodoList.new
   end
 
-  # GET /todo_lists/1/edit
+  
   def edit
   end
 
-  # POST /todo_lists
-  # POST /todo_lists.json
+  
   def create
     @todo_list = TodoList.new(todo_list_params)
 
     respond_to do |format|
       if @todo_list.save
-        format.html { redirect_to @todo_list, notice: 'Todo list was successfully created.' }
+        format.html { redirect_to @todo_list, notice: 'Todo list was created.' }
         format.json { render :show, status: :created, location: @todo_list }
       else
         format.html { render :new }
@@ -37,12 +34,12 @@ class TodoListsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /todo_lists/1
-  # PATCH/PUT /todo_lists/1.json
+ 
+
   def update
     respond_to do |format|
       if @todo_list.update(todo_list_params)
-        format.html { redirect_to @todo_list, notice: 'Todo list was successfully updated.' }
+        format.html { redirect_to @todo_list, notice: 'Todo list was updated.' }
         format.json { render :show, status: :ok, location: @todo_list }
       else
         format.html { render :edit }
@@ -56,7 +53,7 @@ class TodoListsController < ApplicationController
   def destroy
     @todo_list.destroy
     respond_to do |format|
-      format.html { redirect_to todo_lists_url, notice: 'Todo list was successfully destroyed.' }
+      format.html { redirect_to root_url, notice: 'Todo list was destroyed.' }
       format.json { head :no_content }
     end
   end
